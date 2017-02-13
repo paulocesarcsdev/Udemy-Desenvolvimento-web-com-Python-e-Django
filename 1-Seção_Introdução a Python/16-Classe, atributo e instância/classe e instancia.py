f12bcd3959310f0d1321c631c0e7e6fdf596c420
Paulo César <paulocesarcs.dev@gmail.com>
2017-02-13 09:50:11 -0200

    Aula 16

---
+++ 1-Seção_Introdução a Python/16-Classe, atributo e instância/classe e instancia.py
@@ -0,0 +1,13 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> class Pessoa:
+	nome = ""
+	idade = 0
+	mae = ""
+
+	
+>>> #fazer instancia de objetos
+>>> p = Pessoa()
+>>> type(p)
+<class '__main__.Pessoa'>
+>>> 

c9f559d2379295ca0c7df9b266ad74bb31c8c92c
Paulo César <paulocesarcs.dev@gmail.com>
2017-02-13 00:06:18 -0200

    aula 10

---
+++ 1-Seção_Introdução a Python/10-Loops - FOR/10. Loops - FOR.py
@@ -0,0 +1,30 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # E usado quando existe necessidade de fazer interação em uma estrutura ou determinada sequencia, para isso pode ser usando for, ele faz interação em elementos individuais em uma determinada estrtura.
+>>> lang = "Python" #será feita a interação de cada letra da string python
+>>> for l in lang:
+	print(l)
+
+	
+P
+y
+t
+h
+o
+n
+>>> #Pode ser feito a repetição em qualquer estrutura interavel, sequencia numerica, lista, dicionario.
+>>> for n in range(10):
+	print(n)
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> 
